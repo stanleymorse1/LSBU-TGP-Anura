01ab46be948c7e41c8f8ebead1fc5984186e75b8
stanleymorse1 <stanleymorse1@gmail.com>
2022-11-17 22:08:32 +0000

    Imported art assets

---
+++ Anura/Assets/Scripts/CamTrigger.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using UnityEngine.Events;
+
+public class CamTrigger : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 triggerSize;
+    public UnityEvent enter;
+    public UnityEvent exit;
+    bool debounce;
+    int prev;
+
+    private void FixedUpdate()
+    {
+        Collider[] nearColliders = Physics.OverlapBox(transform.position, triggerSize);
+        if (nearColliders.Length > 0)
+            foreach (Collider collider in nearColliders)
+            {
+                if (collider.CompareTag("Player") && !debounce)
+                {
+                    Camera.main.GetComponent<CinemachineBrain>().ActiveVirtualCamera.VirtualCameraGameObject.SetActive(false);
+                    enter.Invoke();
+                    debounce = true;
+                }
+                if (prev > nearColliders.Length)
+                {
+                    debounce = false;
+                    exit.Invoke();
+                    Debug.Log("Exited");
+                }
+                prev = nearColliders.Length;
+            }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(transform.position, triggerSize);
+    }
+}
